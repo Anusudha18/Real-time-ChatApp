ee0ea317cd80e20325cda8820cc9c9778298d6fc
chinnatalli <anusudhamuppalla@gmail.com>
2026-08-07 16:10:04 +0530

    inital commit

---
+++ Backend/src/index.js
@@ -0,0 +1,7 @@
+// importing the express framework
+//const express =require("express");
+import express from "express";
+import "dotenv/config";
+const app=express();
+const PORT=process.env.PORT
+app.listen(PORT,()=>console.log("server is up and running on PORT",port));
