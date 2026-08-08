591b45eb132633c32c6a6c52b0a333c98329c7a1
chinnatalli <anusudhamuppalla@gmail.com>
2026-08-08 08:06:24 +0530

    docker files added

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,6 +8,10 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
+import fs from"fs";  //fs:file system
+import path from "path";
+
+
 import User from "./models/user.model.js"
 import { connectDB } from "./lib/db.js";
 
@@ -16,6 +20,7 @@ import { clerkMiddleware } from '@clerk/express';
 const app=express();
 const PORT=process.env.PORT;
 const FRONTEND_URL=process.env.FRONTEND_URL
+const publicDir =path.join(process.cwd(),"public");
 
 app.use(express.json)  //it is also on eof the middleware ...it used for parse the data and json data
 app.use(cors({origin:FRONTEND_URL,credentials:true}));     // browser security rule
@@ -27,7 +32,15 @@ app.get("/health",(req,res)=>{
     res.status(200).json({ok:true});
 });
 
+// if the public direcrtory exist ,serve the statci files
+//this is for production build
+if (fs.existsSync(publicDir)){
+app.use(express.static(publicDir))
 
+app.get("/{*any}",(req,res,next)=>{
+    res.sendFile(path.join(publicDir,"index.html"),(err)=>next(error));
+});
+}
 
 app.listen(PORT,()=>{
     connectDB();
