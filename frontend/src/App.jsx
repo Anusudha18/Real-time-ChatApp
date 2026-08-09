0777f177f04d215ccd86cacb5d8f43d2136e753e
chinnatalli <anusudhamuppalla@gmail.com>
2026-08-09 16:03:21 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,27 @@
-import './App.css';
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react';
+
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import ChatPage from "./pages/ChatPage";
+import  AuthPage  from "./pages/AuthPage";
+import { Navigate, Route, Routes } from "react-router";
+
 
 
 function App() {
-  return (
-    <>
-      <div>
-        <h1>MY APP</h1>
-      </div>
-
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal"/>    
-          <SignUpButton mode="modal"/>
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </>
-  )
+   return (
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+        <Toaster />
+      </WallpaperProvider>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
