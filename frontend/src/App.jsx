7d8f3820d4e18798492c25559f86e07cf364c664
chinnatalli <anusudhamuppalla@gmail.com>
2026-08-09 19:54:18 +0530

    updating files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,36 @@
-
 import { WallpaperProvider } from "./context/WallpaperContext";
 import { ThemeProvider } from "./context/ThemeContext";
-import ChatPage from "./pages/ChatPage";
-import  AuthPage  from "./pages/AuthPage";
 import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
+import PageLoader from "./components/PageLoader";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
-
+import { Toaster } from "react-hot-toast";
 
 function App() {
-   return (
+  const { isSignedIn, isLoaded } = useAuth();
+
+  // option 1
+  // const { checkAuth, isCheckingAuth, clearAuth } = useAuthStore();
+
+  // option 2 - better for performance
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
+
+  return (
     <ThemeProvider>
       <WallpaperProvider>
         <Routes>
